b1a8178d4605ce3459a4495411fd95def3ab93be
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-19 16:23:46 -0600

    add initial testing for cell class

---
+++ spec/cell_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+RSpec.describe Cell do
+  describe "#cell initialization" do
+    before(:each) do
+      @cell = Cell.new("B4")
+    end
+
+    it 'is a cell' do
+      expect(@cell).to be_an_instance_of Cell
+    end
+    
+    it 'has attributes' do
+      expect(@cell.coordinate).to eq("B4")
+      expect(@cell.ship).to be nil
+    end
+    
+    it 'has an empty status' do
+      expect(@cell.empty?).to be true
+    end
+    
+  end
+  
+  describe "#ship placement" do
+    before(:each) do
+      @cell = Cell.new("B4")
+      @cruiser = Ship.new("Cruiser", 3)
+    end
+  
+    it 'can contain a ship' do
+      expect(@cell.ship).to be nil
+      expect(@cell.empty?).to be true
+      
+      @cell.place_ship(@cruiser)
+      
+      expect(@cell.ship).to eq(@cruiser)
+      expect(@cell.empty?).to be false
+    end
+  end
+end
