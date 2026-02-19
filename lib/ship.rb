e2f6ab7bd6c00263fc7203534c70dfad4871e40d
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-19 16:11:10 -0600

    add ship initialization and hit and sinking capabilities

---
+++ lib/ship.rb
@@ -0,0 +1,19 @@
+class Ship
+  attr_reader :name, :length, :health
+  
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @health = length
+  end
+
+  def hit
+    if health > 0
+      @health -= 1
+    end
+  end
+
+  def sunk?
+    health == 0
+  end
+end
