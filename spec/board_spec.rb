a00c209a92fc88e67a19588b4b300fcfe7fc0527
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-19 18:03:58 -0600

    add initial testing for board and cell hash creation

---
+++ spec/board_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe Board do
+  describe '#initialize' do
+    before(:each) do
+      @board = Board.new
+    end
+    it 'can create a board object' do
+      expect(@board).to be_an_instance_of Board
+    end
+
+    it 'has cells' do
+      expect(@board.cells.keys).to eq(["A1", "A2", "A3", "A4", "B1", "B2",
+      "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"])
+      expect(@board.cells).to be_an_instance_of Hash
+
+      @board.cells.map do |k, v|
+        expect(v).to be_an_instance_of Cell
+      end
+    end
+
+  end
+end
