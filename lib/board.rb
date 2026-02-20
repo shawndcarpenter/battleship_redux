efcfc09ecbfa143b937472e4f70b93fb77e6c1f2
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-19 18:12:04 -0600

    add board initialization and cell creation functionlity to board class

---
+++ lib/board.rb
@@ -0,0 +1,20 @@
+class Board
+  attr_reader :cells
+
+  def initialize
+    @cells = {}
+
+    build_cells
+  end
+
+  def build_cells
+    rows = ["A", "B", "C", "D"]
+    rows.each do |row|
+      i = 0
+      until i == 4
+        i += 1
+        @cells["#{row + i.to_s}"] = Cell.new("#{row + i.to_s}")
+      end
+    end
+  end
+end
