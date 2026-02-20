1330e87646ef033dce666613ce2411ab98548567
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-19 20:22:34 -0600

    add testing for ship placement validation

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -34,4 +34,35 @@ RSpec.describe Board do
       expect(@board.valid_coordinate?("A22")).to be false
     end
   end
+
+  describe '#valid_placement?' do
+    before(:each) do
+      @board = Board.new
+      @cruiser = Ship.new("Cruiser", 3)
+      @submarine = Ship.new("Submarine", 2)
+    end
+
+    it 'can validate coordinates in array match length of ship' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be false
+    end
+    
+    it 'can make sure coordinates are consecutive' do
+      # binding.pry
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be false
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be false
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be false
+    end
+    
+    it 'can validate coordinates are not diagonal' do
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be false
+      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be false
+    end
+
+    it 'can validate placement' do
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be true
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be true
+    end
+  end
 end
