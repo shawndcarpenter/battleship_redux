75b5173c36e0c9c0da487eadb335d9baf7a98fc0
shawndcarpenter <shawncarpenter.co@gmail.com>
2026-02-19 20:23:11 -0600

    add ship placement validation to board class

--- lib/board.rb
+++ lib/board.rb
@@ -21,4 +21,30 @@ class Board
   def valid_coordinate?(coordinate)
     @cells.keys.include?(coordinate)
   end
+
+  def valid_placement?(ship, coordinates)
+    ord_letters = []
+    numbers = []
+    coordinates.each do |coordinate|
+      ord_letters <<coordinate[0].ord
+      numbers <<coordinate[1].to_i
+    end
+
+    (ship.length == coordinates.length) && 
+    (ord_letters.uniq.length == 1 && self.consecutive?(numbers) || 
+    numbers.uniq.length == 1 && self.consecutive?(ord_letters))
+  end
+
+  def consecutive?(numbers)
+    i = numbers[0]
+
+    numbers.each do |number|
+      if i == number
+        i += 1
+      else
+        return false
+      end
+    end
+    true
+  end
 end
